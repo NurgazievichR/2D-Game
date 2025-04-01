7c2780a3aa4306af50dee39e40422713764a0519
NurgazievichR <zalalidinovroma@gmail.com>
2025-04-01 23:36:30 +0600

    convertation images

--- block_info.h
+++ block_info.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <string>
 #include <iostream>
 #include <cstdlib>
 
@@ -10,15 +9,43 @@ struct BlockInfo {
     sf::Texture texture;
     bool passable;
 
-    BlockInfo(const std::string& blockName, const std::string& texturePath, bool isPassable=false)
+    BlockInfo(const std::string& blockName, const std::string& texturePath, bool isPassable = false)
         : name(blockName), passable(isPassable)
     {
         std::string fullPath = "media/blocks/" + texturePath;
-        if (!texture.loadFromFile(fullPath)) {
-            std::cerr << "Ошибка загрузки текстуры: " << fullPath << std::endl;
+        sf::Image img;
+        if (!img.loadFromFile(fullPath)) {
+            std::cerr << "Ошибка загрузки изображения: " << fullPath << std::endl;
+            exit(EXIT_FAILURE);
+        }
+
+        // Приведение изображения к 65x65
+        img = resizeImage(img, 65, 65);
+
+        if (!texture.loadFromImage(img)) {
+            std::cerr << "Ошибка создания текстуры из изображения: " << fullPath << std::endl;
             exit(EXIT_FAILURE);
         }
     }
-    BlockInfo()=default;
+
+    BlockInfo() : name(""), passable(true) { }
+
+private:
+    static sf::Image resizeImage(const sf::Image& original, unsigned targetWidth, unsigned targetHeight) {
+        sf::Image resized;
+        resized.create(targetWidth, targetHeight);
+
+        sf::Vector2u srcSize = original.getSize();
+        for (unsigned y = 0; y < targetHeight; ++y) {
+            for (unsigned x = 0; x < targetWidth; ++x) {
+                // Находим соответствующий пиксель из оригинала
+                unsigned srcX = x * srcSize.x / targetWidth;
+                unsigned srcY = y * srcSize.y / targetHeight;
+                resized.setPixel(x, y, original.getPixel(srcX, srcY));
+            }
+        }
+
+        return resized;
+    }
 };
 
